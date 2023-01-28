37589febe77f7ec604565b2c0caad89d9ef67532
Yordi <yoriyordina7@gmail.com>
2023-01-29 00:26:11 +0300

    save

--- src/index.js
+++ src/index.js
@@ -1,59 +1,19 @@
-/* eslint-disable no-unused-vars */
-/* eslint func-names: ["error", "never", { "generators": "always" }] */
-const myNodelist = document.getElementsByTagName('LI');
-let i;
-/* eslint-disable-next-line no-plusplus */
-for (i = 0; i < myNodelist.length; i++) {
-  const span = document.createElement('SPAN');
-  const txt = document.createTextNode('\u00D7');
-  span.className = 'close';
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+import './style.css';
+import Page from './info.js';
 
-const close = document.getElementsByClassName('close');
-/* eslint-disable-next-line no-plusplus */
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function () {
-    const div = this.parentElement;
-    div.style.display = 'none';
-  };
-}
+const todotask = new Page();
+todotask.display();
 
-const list = document.querySelector('ul');
-list.addEventListener('click', (ev) => {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+document.querySelector('#form-sector').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const activity = e.target.elements.activity.value;
+  todotask.addWork(activity);
+  e.target.reset();
+});
+document.querySelector('.clear-all').addEventListener('click', () => {
+  todotask.clearCompletedActivity();
+});
 
-function newElement() {
-  const li = document.createElement('li');
-  const inputValue = document.getElementById('myInput').value;
-  const t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert('You must write something!');
-  } else {
-    document.getElementById('myUL').appendChild(li);
-  }
-  document.getElementById('myInput').value = '';
-
-  const span = document.createElement('SPAN');
-  const txt = document.createTextNode('\u00D7');
-  span.className = 'close';
-  span.appendChild(txt);
-  li.appendChild(span);
-  /* eslint-disable-next-line no-plusplus */
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      const div = this.parentElement;
-      div.style.display = 'none';
-    };
-  }
-}
-
-function removeAll() {
-  const lst = document.getElementsByTagName('ul');
-  lst[0].innerHTML = '';
-}
+document.querySelector('#delete-all').addEventListener('click', () => {
+  todotask.clearAll();
+});
